b11c1fea248a7f9e2444b44f370db8917b005bad
Vartan Shirinyan <02vartan-s@mail.ru>
2022-12-01 23:40:39 +0300

    Нахождение наибольшего числа из 3 чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter Three Numbers (Введите три числа): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", a);
+    }
+    else
+    {
+        Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", c);
+    }
+}
+else if (b > c)
+{
+    Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", b);
+}
+else
+{
+    Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", c);
+}
+Console.ReadLine();
