9d6180c5a6e1c4f4011d0adbafd6e6b0031a1af2
Vartan Shirinyan <02vartan-s@mail.ru>
2022-12-01 23:33:18 +0300

    Нахождение наибольшего и наименьшего числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the Two Numbers (Введите два числа): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", a);
+}
+else
+{
+    Console.WriteLine("{0} is the Greatest Number (является наибольшим числом)", b);
+}
+if (a < b)
+{
+    Console.WriteLine("{0} is the smallest number (является наименьшим числом)", a);
+}
+else
+{
+    Console.WriteLine("{0} is the smallest number (является наименьшим числом)", b);
+}
+Console.ReadLine();
