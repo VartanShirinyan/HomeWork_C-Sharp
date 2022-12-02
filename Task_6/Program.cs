7dcbcc2235321bf95b1bb992cf07d2f0443b28d1
Vartan Shirinyan <02vartan-s@mail.ru>
2022-12-02 10:26:22 +0300

    Checking division by 2 (Проверка деления на 2)

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a Number (Введите число): ");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.WriteLine("Divisible by 2 (Делится на 2)");
+}
+else
+{
+    Console.WriteLine("Not divisible by 2 (Не делится на 2)");
+}
+Console.ReadLine();
