f9497af4009cfdb35b744678cd38a2cd5869506a
Vartan Shirinyan <02vartan-s@mail.ru>
2022-12-02 12:48:24 +0300

    Even numbers from 1 to N (четные числа от 1 до N)

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a positive number (Введите положительное число)");
+int i = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= i)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
+Console.ReadLine();
